5051eef3956b875f956c40e0476ad9b21addff02
Hiromu OCHIAI <otiai10@gmail.com>
2020-04-30 04:46:26 +0200

    Support dispatching methods by JSON file names

--- src/endpoint/json.ts
+++ src/endpoint/json.ts
@@ -8,6 +8,25 @@ export default class JSONEndpoint extends Endpoint {
     const parsed = path.parse(fpath);
     const restdir = path.dirname(fpath.replace(rootDir, ''));
     const restpath = (parsed.name == 'index') ? restdir : path.join(restdir, parsed.name);
-    super(Method.GET, restpath, (req, res) => res.json(require(fpath)));
+    const entry = require(fpath);
+    switch (parsed.name.toLowerCase()) {
+    case 'index':
+        super((entry.__http_method || Method.GET).toUpperCase(), restdir, (req, res) => res.json(entry));
+        break;
+    case '_get':
+        super(Method.GET, restdir, (req, res) => res.json(entry));
+        break;
+    case '_post':
+        super(Method.POST, restdir, (req, res) => res.json(entry));
+        break;
+    case '_delete':
+        super(Method.DELETE, restdir, (req, res) => res.json(entry));
+        break;
+    case '_put':
+        super(Method.PUT, restdir, (req, res) => res.json(entry));
+        break;
+    default:
+        super(Method.GET, restpath, (req, res) => res.json(entry));
+    }
   }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -7,6 +7,7 @@ describe("App", () => {
     const out = new Stream();
     const app = await App({ rootDir: "./example/root", out });
     expect(out.toString().split("\n")).toStrictEqual([
+      "PUT	/foo",
       "GET	/foo/baa",
       "GET	/hello/:name",
       "GET	/users/:id",
