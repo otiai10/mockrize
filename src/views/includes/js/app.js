4d7b33eb803be3bbba448656db7d594bf9bdc04b
Hiromu Ochiai <otiai10@gmail.com>
2020-05-02 17:26:21 +0900

    Syntax Highlight for Expanded Code

--- src/views/includes/js/app.js
+++ src/views/includes/js/app.js
@@ -5,7 +5,11 @@
       const url = target.getAttribute('data-raw-url');
       const text = await (await fetch(url)).text();
       const dest = scope.document.querySelector(`div.code-expandable[data-raw-url="${url}"]`);
-      dest.innerHTML = `<pre class="bg-light code"><code>${text}</code></pre>`;
+      const pre = scope.document.createElement('pre');
+      pre.className = 'bg-light code';
+      pre.innerHTML = `<code>${text}</code>`;
+      scope.hljs.highlightBlock(pre);
+      dest.appendChild(pre);
       target.setAttribute('data-loaded', 'true');
     }
   };
